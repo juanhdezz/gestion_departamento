faee4e72b7673fdeee49b2c10a0d096f983658b5
Juan Hernández <jhernandezsanchezagesta@gmail.com>
2025-01-02 20:37:21 +0100

    Añadimos dependencias para docuemntación

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -6,6 +6,69 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $dni
+ * @property string $full_name
+ * @property string|null $short_name
+ * @property string $email
+ * @property string $login
+ * @property string $password
+ * @property string|null $remember_token
+ * @property string $user_type
+ * @property string|null $office
+ * @property string|null $office_phone
+ * @property string|null $personal_phone
+ * @property string|null $copier_uid
+ * @property string|null $copier_password
+ * @property int $teaches
+ * @property int $is_current_member
+ * @property int $is_council_member
+ * @property int $is_total_member
+ * @property string $last_access
+ * @property string|null $group
+ * @property string|null $category
+ * @property string|null $website
+ * @property int|null $teaching_order
+ * @property string|null $scholarship_history
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\ProfessorFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereCopierPassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereCopierUid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereDni($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereFullName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereGroup($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereIsCouncilMember($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereIsCurrentMember($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereIsTotalMember($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereLastAccess($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereLogin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereOffice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereOfficePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor wherePersonalPhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereScholarshipHistory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereShortName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereTeaches($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereTeachingOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereUserType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Professor whereWebsite($value)
+ * @mixin \Eloquent
+ */
 class Professor extends Authenticatable
 {
     use HasFactory, Notifiable;
